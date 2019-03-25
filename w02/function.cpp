2ddd497a0140f8e3a17ba4e11d15c849352cb4e0
CallumHoward <callumh@cse.unsw.edu.au>
2019-03-26 07:43:14 +1100

    added week2 exercises

---
+++ w02/function.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int max_of_four(int a, int b, int c, int d);
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+
+    return 0;
+}
+
+int max_of_four(int a, int b, int c, int d) {
+    return std::max({a, b, c, d});
+}
+
---
+++ w02/structs.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+struct Student {
+    int age;
+    std::string first_name;
+    std::string last_name;
+    int standard;
+};
+
+int main() {
+    Student st;
+
+    std::cin >> st.age >> st.first_name >> st.last_name >> st.standard;
+    std::cout << st.age << " " << st.first_name << " " << st.last_name << " " << st.standard;
+
+    return 0;
+}
