3aea1859b7a891ebf8953b883525f2e9f0447751
CallumHoward <callumh@cse.unsw.edu.au>
2019-04-02 08:54:32 +1100

    update with debug macro example

--- w01/hello.hpp
+++ w01/hello.hpp
@@ -3,10 +3,18 @@
 
 #define GREETING "Hello World from macro\n"
 
+//#define DEBUG
+
 // function prototype
 void foo();
 
 // function definition
 void foo() {
     std::cout << "I am a function defined in the header file\n";
+
+#ifdef DEBUG
+    std::cout << "Doing some debug logging!" << '\n';
+#else
+    std::cout << "Release version!" << '\n';
+#endif
 }
